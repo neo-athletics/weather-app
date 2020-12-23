27e2fe4f54539ac34e90abe8dbe280e1b913793e
Juan Rodriguez <juan109rodriguez@gmail.com>
2020-12-23 02:17:01 -0600

    removed some imports that weren't used

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import FollowingDays from "./FollowingDays";
 import CurrentDay from "./CurrentDay";
 import axios from "axios";
